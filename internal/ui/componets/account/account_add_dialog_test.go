436204e7766b2db96d79a0d00df0e652d83c36a6
Nelson Marro <nelsonmarro99@gmail.com>
2025-06-27 19:30:08 -0500

    /workspace/github/nelsonmarro/accountable-holo

--- internal/ui/componets/account/account_add_dialog_test.go
+++ internal/ui/componets/account/account_add_dialog_test.go
@@ -13,7 +13,7 @@ func TestHandleSubmit(t *testing.T) {
 	t.Run("should call service and trigger callback on success", func(t *testing.T) {
 		// Arrange
 		var wg sync.WaitGroup
-		wg.Add(1) // We are waiting for one event: the callback to be called.
+		wg.Add(1)
 
 		callbackFired := false
 		testCallback := func() {
@@ -24,14 +24,12 @@ func TestHandleSubmit(t *testing.T) {
 		d, mockService := setupTest(testCallback)
 
 		// The mock service now just needs to return success.
-		// We no longer use .Run() to signal completion here.
 		mockService.On("CreateNewAccount", mock.Anything, mock.AnythingOfType("*domain.Account")).Return(nil)
 
 		// Act
 		d.handleSubmit(true)
 
 		// Assert
-		// This now correctly waits until the callbackAction goroutine has finished.
 		waitTimeout(t, &wg, 2*time.Second)
 
 		mockService.AssertExpectations(t)
