ebe6d5c96c4ca7b4c51ad738060613197676e690
Nelson Marro <nelsonmarro99@gmail.com>
2025-06-28 15:20:11 -0500

    /workspace/github/nelsonmarro/accountable-holo

--- internal/ui/componets/account/account_edit_dialog_test.go
+++ internal/ui/componets/account/account_edit_dialog_test.go
@@ -3,23 +3,16 @@ package account
 import (
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/nelsonmarro/accountable-holo/internal/domain"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 func TestFetchAccount(t *testing.T) {
 	t.Run("should fire onSuccess callback if service works", func(t *testing.T) {
 		// Arrange
-		expectedAccount := &domain.Account{
-			BaseEntity: domain.BaseEntity{
-				ID: 1,
-			},
-			Name:           "Account 1",
-			Number:         "11111",
-			InitialBalance: 10,
-			Type:           domain.OrdinaryAccount,
-		}
 		var wg sync.WaitGroup
 		wg.Add(1)
 
@@ -40,12 +33,19 @@ func TestFetchAccount(t *testing.T) {
 		d, mockService := setupTestEditDialog(editCallback)
 
 		mockService.On("GetAccountByID", mock.Anything, d.accountID).
-			Return(expectedAccount).
+			Return(&domain.Account{}, nil).
 			Run(func(args mock.Arguments) {
 				wg.Done()
 			})
 
 		// Act
 		d.fetchAccount(onSuccessCallback, onFailureCallback)
+
+		// Assert
+		waitTimeout(t, &wg, 1*time.Second)
+
+		mockService.AssertExpectations(t)
+		assert.True(t, *onSuccessFires)
+		assert.False(t, *onFailureFires)
 	})
 }
