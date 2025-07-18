d640e24e22aaa2825521c85519b5f37ab5500b1e
Nelson Marro <nelsonmarro99@gmail.com>
2025-07-18 11:40:05 -0500

    /home/nelson/workspace/nelsonmarro/accountable-holo

--- internal/ui/componets/hoverable_hyperlink.go
+++ internal/ui/componets/hoverable_hyperlink.go
@@ -1,7 +1,10 @@
 package componets
 
 import (
+	"net/url"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -10,4 +13,22 @@ type HoverableHyperLink struct {
 	TooltipText string
 	canvas      fyne.Canvas
 	popup       *widget.PopUp
+}
+
+func NewHoverableHyperLink(text string, url *url.URL, canvas fyne.Canvas) *HoverableHyperLink {
+	h := &HoverableHyperLink{
+		canvas: canvas,
+	}
+	h.ExtendBaseWidget(h)
+	h.SetText(text)
+	h.SetURL(url)
+	return h
+}
+
+// MouseIn is called when the mouse enters the widget area.
+func (h *HoverableHyperLink) MouseIn(_ *desktop.MouseEvent) {
+	if h.TooltipText != "" && h.popup == nil {
+		label := widget.NewLabel(h.TooltipText)
+		h.popup = widget.NewPopUp(label, h.canvas)
+	}
 }
