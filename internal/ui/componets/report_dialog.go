53917fd73dfb0207ce146fc0543c406c8026e9bf
Nelson Marro <nelsonmarro99@gmail.com>
2025-07-23 18:40:04 -0500

    /home/nelson/workspace/nelsonmarro/accountable-holo

--- internal/ui/componets/report_dialog.go
+++ internal/ui/componets/report_dialog.go
@@ -1,1 +1,20 @@
 package componets
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+	"github.com/nelsonmarro/accountable-holo/internal/domain"
+)
+
+type ReportDialog struct {
+	parentWindow     fyne.Window
+	startDateEntry   *widget.DateEntry
+	endDateEntry     *widget.DateEntry
+	categorySelect   *widget.SelectEntry
+	typeSelect       *widget.SelectEntry
+	descriptionEntry *widget.Entry
+	allCategories    []domain.Category
+	applyCallback    func(filters domain.TransactionFilters)
+	dialog           dialog.Dialog
+}
