48637c49db3887c37324f7c4d95b49b6fc849c3d
Nelson Marro <nelsonmarro99@gmail.com>
2025-07-25 16:30:04 -0500

    /home/nelson/workspace/nelsonmarro/accountable-holo

--- internal/ui/componets/report_dialog.go
+++ internal/ui/componets/report_dialog.go
@@ -1,6 +1,8 @@
 package componets
 
 import (
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
@@ -10,15 +12,20 @@ type ReportDialog struct {
 	parentWindow fyne.Window
 	formatSelect *widget.Select
 	dialog       dialog.Dialog
-	onGenerate   func(format string)
+	onGenerate   func(format string, outputPath string)
 }
 
-func NewReportDialog(parentWindow fyne.Window, onGenerate func(format string)) *ReportDialog {
+func NewReportDialog(parentWindow fyne.Window, onGenerate func(format string,
+	outputPath string),
+) *ReportDialog {
 	rd := &ReportDialog{
 		parentWindow: parentWindow,
 		onGenerate:   onGenerate,
 	}
+	return rd
+}
 
+func (rd *ReportDialog) Show() {
 	rd.formatSelect = widget.NewSelect([]string{"PDF", "CSV"}, nil)
 	rd.formatSelect.SetSelected("PDF") // Default selection
 
@@ -30,7 +37,22 @@ func NewReportDialog(parentWindow fyne.Window, onGenerate func(format string)) *
 		if !confirmed {
 			return
 		}
-		rd.onGenerate(rd.formatSelect.Selected)
+		fileSaveDialog := dialog.NewFileSave(func(writer fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, rd.parentWindow)
+				return
+			}
+
+			if writer == nil { // User canceled the save dialog}
+				return
+			}
+			defer writer.Close()
+
+			rd.onGenerate(rd.formatSelect.Selected, writer.URI().Path())
+		}, rd.parentWindow)
+
+		fileSaveDialog.SetFileName("reporte." + strings.ToLower(rd.formatSelect.Selected))
+		fileSaveDialog.Show()
 	}
 
 	rd.dialog = dialog.NewForm(
@@ -41,11 +63,6 @@ func NewReportDialog(parentWindow fyne.Window, onGenerate func(format string)) *
 		callback,
 		rd.parentWindow,
 	)
-
-	return rd
-}
-
-func (rd *ReportDialog) Show() {
 	rd.dialog.Resize(fyne.NewSize(300, 200))
 	rd.dialog.Show()
 }
