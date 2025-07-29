e7c4eb9f6e9fc475b1f584f29350fb019d6eea9a
Nelson Marro <nelsonmarro99@gmail.com>
2025-07-29 07:30:04 -0500

    /home/nelson/workspace/nelsonmarro/accountable-holo

--- internal/application/report/pdf_generator.go
+++ internal/application/report/pdf_generator.go
@@ -11,6 +11,7 @@ import (
 	"github.com/johnfercher/maroto/v2/pkg/components/row"
 	"github.com/johnfercher/maroto/v2/pkg/components/text"
 	"github.com/johnfercher/maroto/v2/pkg/consts/align"
+	"github.com/johnfercher/maroto/v2/pkg/consts/breakline"
 	"github.com/johnfercher/maroto/v2/pkg/consts/fontstyle"
 	"github.com/johnfercher/maroto/v2/pkg/core"
 	"github.com/johnfercher/maroto/v2/pkg/props"
@@ -95,7 +96,7 @@ func (g *PDFReportGenerator) buildTransactionsTable(m core.Maroto, transactions 
 		}
 
 		amountStyle := props.Text{
-			Align: align.Right,
+			Align: align.Center,
 			Top:   2,
 		}
 		if tx.Category.Type == domain.Income {
@@ -104,15 +105,21 @@ func (g *PDFReportGenerator) buildTransactionsTable(m core.Maroto, transactions 
 			amountStyle.Color = &props.RedColor
 		}
 
+		descriptionStyle := props.Text{
+			Align:             align.Justify,
+			Top:               2,
+			BreakLineStrategy: breakline.EmptySpaceStrategy,
+		}
+
 		// Add a row for the transaction data
 		m.AddRow(10,
 			col.New(2).Add(text.New(tx.TransactionDate.Format("2006-01-02"), props.Text{Align: align.Center, Top: 2})),
 			col.New(1).Add(text.New(tx.TransactionNumber, props.Text{Align: align.Center, Top: 2})),
-			col.New(3).Add(text.New(tx.Description, props.Text{Align: align.Left, Top: 2})),
+			col.New(3).Add(text.New(tx.Description, descriptionStyle)),
 			col.New(2).Add(text.New(categoryName, props.Text{Align: align.Center, Top: 2})),
 			col.New(1).Add(text.New(categoryType, props.Text{Align: align.Center, Top: 2})),
 			col.New(2).Add(text.New(fmt.Sprintf("%.2f", tx.Amount), amountStyle)),
-			col.New(1).Add(text.New(fmt.Sprintf("%.2f", tx.RunningBalance), props.Text{Align: align.Right, Top: 2})),
+			col.New(1).Add(text.New(fmt.Sprintf("%.2f", tx.RunningBalance), props.Text{Align: align.Center, Top: 2})),
 		)
 
 		// Add a separator line
