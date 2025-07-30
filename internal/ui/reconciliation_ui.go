380c9a0b5e654835dcef36dc9751668b4df1f03d
Nelson Marro <54252208+nelsonmarro@users.noreply.github.com>
2025-07-30 15:40:03 -0500

    /home/nelson/workspace/nelsonmarro/accountable-holo

--- internal/ui/reconciliation_ui.go
+++ internal/ui/reconciliation_ui.go
@@ -9,7 +9,22 @@ func (ui *UI) makeReconciliationUI() fyne.CanvasObject {
 	// UI elelennts
 	title := widget.NewLabel("Reconciliación de Cuentas")
 
-	// containers
+	// Top card (form)
+	ui.makeFormCard()
 
+	// Bottom card (reconciliation statement)
+	ui.makeStatementCard()
+
+	// containers
 	return widget.NewLabel("Reconciliation UI is under construction. Please check back later.")
+}
+
+func (ui *UI) makeFormCard() fyne.CanvasObject {
+	accountsSelector := widget.NewSelect([]string{}, nil) // we'll populate this later
+	endingDateEntry := widget.NewDateEntry()
+	actualBalanceEntry := widget.NewEntry()
+}
+
+func (ui *UI) makeStatementCard() fyne.CanvasObject {
+	return widget.NewLabel("Statement Card is under construction. Please check back later.")
 }
