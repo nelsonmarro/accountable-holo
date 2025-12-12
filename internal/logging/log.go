2a29cb252ce3004e03db3eb49f72f52c63cbe63c
Nelson Marro <54252208+nelsonmarro@users.noreply.github.com>
2025-12-12 13:20:03 -0500

    /home/nelson/workspace/nelsonmarro/accountable-holo

--- internal/logging/log.go
+++ internal/logging/log.go
@@ -1,7 +1,25 @@
 // package logging provides logging utilities for the application.
 package logging
 
-import "log"
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
 
 func Init() (*log.Logger, *log.Logger, error) {
+	executablePath, err := os.Executable()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	dirs := filepath.Dir(executablePath)
+	logsDirPath := filepath.Join(dirs, "logs")
+
+	os.MkdirAll(logsDirPath, 0o755)
+
+	logFileName := time.Now().Format("2006-01-02")
+	newLogFile := filepath.Join(logsDirPath, logFileName)
+	err := os.OpenFile(newLogFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
 }
